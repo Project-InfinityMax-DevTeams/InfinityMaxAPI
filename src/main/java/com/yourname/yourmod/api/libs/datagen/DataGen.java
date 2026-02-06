e2a0902cbd5a6de2475ec00b47e186694a8dd1de
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 19:32:45 +0900

    DataGen.java を更新

--- src/main/java/com/yourname/yourmod/api/libs/datagen/DataGen.java
+++ src/main/java/com/yourname/yourmod/api/libs/datagen/DataGen.java
@@ -1,4 +1,12 @@
+package com.yourname.yourmod.api.libs.datagen;
+
+import com.yourname.yourmod.api.datagen.BlockGen;
+import com.yourname.yourmod.api.datagen.ItemGen;
+import com.yourname.yourmod.api.datagen.EntityGen;
+
 public final class DataGen {
+
+    private DataGen() {}
 
     public static BlockGen block(String id) {
         return new BlockGen(id);
