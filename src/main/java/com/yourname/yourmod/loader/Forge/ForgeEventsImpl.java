bcb68b0552c70711e243cff6ba772fa3f32b2478
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 13:30:55 +0900

    ForgeEventsImpl.java を作成

---
+++ src/main/java/com/yourname/yourmod/loader/Forge/ForgeEventsImpl.java
@@ -0,0 +1,15 @@
+package com.yourname.yourmod.loader.forge;
+
+import com.yourname.yourmod.event.CommonEvents;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+public final class ForgeEventsImpl {
+
+    public static void register(CommonEvents events) {
+        MinecraftForge.EVENT_BUS.addListener(
+                (PlayerEvent.PlayerLoggedInEvent e) ->
+                        events.onPlayerJoin(e.getEntity())
+        );
+    }
+}
