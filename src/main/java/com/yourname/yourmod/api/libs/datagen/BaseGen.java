af7ef4aed645facaeda5fea11f0b7eaef027d15f
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 18:33:15 +0900

    BaseGen.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/libs/datagen/BaseGen.java
@@ -0,0 +1,19 @@
+public abstract class BaseGen<T extends BaseGen<T>> {
+
+    protected final String id;
+
+    protected BaseGen(String id) {
+        this.id = id;
+    }
+
+    protected abstract void submit();
+
+    @SuppressWarnings("unchecked")
+    protected T self() {
+        return (T) this;
+    }
+
+    public void end() {
+        submit();
+    }
+}
