8064c81f6f5c45caa2bab711e1df98435678cbdb
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 19:44:20 +0900

    Events.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/libs/event/Events.java
@@ -0,0 +1,27 @@
+package com.yourname.yourmod.api.libs.event;
+
+import com.yourname.yourmod.api.event.ModEvent;
+import com.yourname.yourmod.api.event.EventPriority;
+
+/**
+ * イベントDSLのエントリポイント
+ * MOD作者は基本ここしか触らない
+ */
+public final class Events {
+
+    private Events() {}
+
+    /**
+     * 汎用イベント登録
+     */
+    public static <T extends ModEvent> EventBuilder<T> on(Class<T> eventClass) {
+        return new EventBuilder<>(eventClass);
+    }
+
+    /**
+     * よく使うイベントのショートカット例
+     */
+    public static EventBuilder<com.yourname.yourmod.api.event.PlayerJoinEvent> playerJoin() {
+        return on(com.yourname.yourmod.api.event.PlayerJoinEvent.class);
+    }
+}
