5b24d9a5497869ee258e14d5cffd076847d83275
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 13:20:29 +0900

    ModEvents.java を更新

--- src/main/java/com/yourname/yourmod/api/event/ModEvents.java
+++ src/main/java/com/yourname/yourmod/api/event/ModEvents.java
@@ -1,13 +1,12 @@
 package com.yourname.yourmod.api.event;
 
-/**
- * MODイベント初期化クラス
- */
+import com.yourname.yourmod.loader.Platform;
+
 public final class ModEvents {
 
     private ModEvents() {}
 
     public static void init() {
-        // TODO: loader側で Forge/Fabric イベントを登録
+        Platform.get().registerEvents();
     }
 }
