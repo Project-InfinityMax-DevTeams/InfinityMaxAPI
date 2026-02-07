7854c027addbfb80e0e948045e48c12f9a7847d7
Yuyuto <yukkurigame0101@gmail.com>
2026-02-07 10:36:41 +0900

    ClientEntrypoint.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/client/ClientEntrypoint.java
@@ -0,0 +1,13 @@
+package com.yourname.yourmod.api.client;
+
+public final class ClientEntrypoint {
+
+    private ClientEntrypoint() {}
+
+    public static void init() {
+        ClientEvents.register();
+        ClientRenderers.register();
+        ClientScreens.register();
+        ClientInputs.register();
+    }
+}
