eaf70d75c287b7adea1852c8dfaa6283c2c4bfef
Yuyuto <yukkurigame0101@gmail.com>
2026-02-17 22:10:16 +0900

    ItemBuilder.java を更新

--- src/main/java/com/yourname/yourmod/api/libs/internal/ItemBuilder.java
+++ src/main/java/com/yourname/yourmod/api/libs/internal/ItemBuilder.java
@@ -2,41 +2,44 @@ package com.yourname.yourmod.api.libs.internal;
 
 import com.yourname.yourmod.api.libs.ModRegistries;
 
-public final class ItemBuilder {
+public final class ItemBuilder<T, TAB> {
 
     private final String id;
-    private Object template;
+    private T template;
     private int stack = 64;
-    private Object tab = null;
+    private TAB tab;
     private int durability = 0;
 
     public ItemBuilder(String id) {
         this.id = id;
     }
 
-    public ItemBuilder stack(int size) {
+    public ItemBuilder<T, TAB> stack(int size) {
         this.stack = size;
         return this;
     }
 
-    public ItemBuilder tab(Object tab) {
+    public ItemBuilder<T, TAB> tab(TAB tab) {
         this.tab = tab;
         return this;
     }
 
-    public ItemBuilder durability(int value) {
+    public ItemBuilder<T, TAB> durability(int value) {
         this.durability = value;
         return this;
     }
 
-    public ItemBuilder template(Object template) {
+    public ItemBuilder<T, TAB> template(T template) {
         this.template = template;
         return this;
     }
 
-    public Object build() {
-        Object item = template != null ? template : new Object();
-        ModRegistries.registerItem(id, item);
-        return item;
+    public T build() {
+        if (template == null) {
+            throw new IllegalStateException("Template required");
+        }
+
+        ModRegistries.registerItem(id, template);
+        return template;
     }
 }
