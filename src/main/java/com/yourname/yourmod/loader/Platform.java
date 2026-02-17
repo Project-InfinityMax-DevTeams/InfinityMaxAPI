3c7b002d60fa94372ffc54fc056385806eb24c47
Yuyuto <yukkurigame0101@gmail.com>
2026-02-18 07:52:51 +0900

    Platform.java を更新

--- src/main/java/com/yourname/yourmod/loader/Platform.java
+++ src/main/java/com/yourname/yourmod/loader/Platform.java
@@ -1,9 +1,8 @@
 package com.yourname.yourmod.loader;
 
-import com.yourname.yourmod.api.libs.internal.*;
 public final class Platform {
-
     private static LoaderExpectPlatform instance;
+
     private Platform() {}
 
     public static void set(LoaderExpectPlatform platform) {
@@ -15,31 +14,5 @@ public final class Platform {
             throw new IllegalStateException("Platform not initialized");
         }
         return instance;
-    }
-
-    public static Platform get() {
-        return INSTANCE;
-    }
-
-    public Registries registries() {
-        return registries;
-    }
-
-    public static final class Registries {
-        public <T> void item(String id, T item) {
-            //
-        }
-
-        public <T> void block(String id, T block) {
-            //
-        }
-
-        public <T, C> void entity(String id, T entity, C category, float width, float height) {
-            //
-        }
-
-        public <T, B> void blockEntity(String id, T entity, B... blocks) {
-            //
-        }
     }
 }
