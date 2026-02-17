81ecaf1788dd35850374e773ca06a7502aaca898
Yuyuto <yukkurigame0101@gmail.com>
2026-02-18 07:28:37 +0900

    Platform.java を更新

--- src/main/java/com/yourname/yourmod/loader/Platform.java
+++ src/main/java/com/yourname/yourmod/loader/Platform.java
@@ -28,19 +28,19 @@ public final class Platform {
 
     public static final class Registries {
         public <T> void item(String id, T item) {
-            Platform.get().registries().item(id, item);
+            //
         }
 
         public <T> void block(String id, T block) {
-            Platform.get().registries().block(id, block);
+            //
         }
 
         public <T, C> void entity(String id, T entity, C category, float width, float height) {
-            Platform.get().registries().entity(id, entityType, category, width, height);
+            //
         }
 
         public <T, B> void blockEntity(String id, T entity, B... blocks) {
-            Platform.get().registries().blockEntity(id, blockEntityType, blocks);
+            //
         }
     }
 }
