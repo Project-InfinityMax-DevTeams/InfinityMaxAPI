4442507c9d7d5c6ff3715b14f2e547e28b58173e
Yuyuto <yukkurigame0101@gmail.com>
2026-02-18 17:05:53 +0900

    ModRegistries.java を更新

--- src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
+++ src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
@@ -15,7 +15,7 @@ public final class ModRegistries {
     }
 
     public static <T> void registerBlock(String id, T block, float strength, boolean noOcclusion) {
-        platformRegistries.block(id, block,strength,noOcculusion);
+        platformRegistries.block(id, block,strength,noOcclusion);
     }
 
     public static <T, C> void registerEntity(String id, T entityType, C category, float width, float height) {
