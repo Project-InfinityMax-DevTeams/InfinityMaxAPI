99d30848bcc6541ee98a088729b9214db9efb554
Yuyuto <yukkurigame0101@gmail.com>
2026-02-24 08:27:35 +0900

    PIMXStorage.java を更新

--- src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXStorage.java
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXStorage.java
@@ -4,9 +4,6 @@ import java.util.Map;
 
 public interface PIMXStorage {
 
-    Object raw = data.type().serialize(data.value());
-    T value = type.deserialize(rawFromFile);
-
     void save(Map<PIMXKey, PIMXData<?>> data);
 
     Map<PIMXKey, PIMXData<?>> load();
