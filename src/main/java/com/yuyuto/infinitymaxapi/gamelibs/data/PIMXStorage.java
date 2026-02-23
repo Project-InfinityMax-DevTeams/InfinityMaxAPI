a5162c0ca6d6bed1c96698f87a760fc2ed4ca502
Yuyuto <yukkurigame0101@gmail.com>
2026-02-24 07:48:25 +0900

    PIMXStorage.java を更新

--- src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXStorage.java
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXStorage.java
@@ -4,6 +4,10 @@ import java.util.Map;
 
 public interface PIMXStorage {
 
+    private boolean dirty;
+    dirty = true;
+    if (!dirty) return;
+
     void save(Map<PIMXKey, PIMXData<?>> data);
 
     Map<PIMXKey, PIMXData<?>> load();
