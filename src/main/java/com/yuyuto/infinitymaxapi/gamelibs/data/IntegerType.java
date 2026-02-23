695637b65bdf4c001ddcd8dc0e0154dd311aed73
Yuyuto <yukkurigame0101@gmail.com>
2026-02-24 08:06:32 +0900

    IntegerType.java を作成

---
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/IntegerType.java
@@ -0,0 +1,27 @@
+package com.yuyuto.infinitymaxapi.gamelibs.data;
+
+public class IntegerType implements PIMXType<Integer> {
+
+    @Override
+    public boolean isValid(Object value) {
+        return value instanceof Integer;
+    }
+
+    @Override
+    public Object serialize(Integer value) {
+        return value; // JSONではそのまま数値
+    }
+
+    @Override
+    public Integer deserialize(Object raw) {
+        if (raw instanceof Number number) {
+            return number.intValue();
+        }
+        throw new PIMXException("Cannot deserialize to Integer");
+    }
+
+    @Override
+    public Integer cast(Object value) {
+        return (Integer) value;
+    }
+}
