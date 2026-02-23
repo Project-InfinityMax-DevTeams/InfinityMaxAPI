bd07dd2c0cd8baaf54cd619f3c6b4424762c0127
Yuyuto <yukkurigame0101@gmail.com>
2026-02-24 08:09:12 +0900

    PIMXTypes.java を作成

---
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXTypes.java
@@ -0,0 +1,9 @@
+package com.yuyuto.infinitymaxapi.gamelibs.data;
+
+public final class PIMXTypes {
+
+    public static final PIMXType<Integer> INTEGER = new IntegerType();
+    public static final PIMXType<String> STRING = new StringType();
+
+    private PIMXTypes() {}
+}
