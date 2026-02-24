15b8eddb2a93015c9565de08f2c3b2f90011933f
Yuyuto <yukkurigame0101@gmail.com>
2026-02-24 16:31:01 +0900

    PIMXTypes.java を更新

--- src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXTypes.java
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/data/PIMXTypes.java
@@ -2,8 +2,24 @@ package com.yuyuto.infinitymaxapi.gamelibs.data;
 
 public final class PIMXTypes {
 
-    public static final PIMXType<Integer> INTEGER = new IntegerType();
-    public static final PIMXType<String> STRING = new StringType();
+    private static final Map<String, PIMXType<?>> TYPES = new HashMap<>();
+
+    public static final PIMXType<Integer> INTEGER = register(new IntegerType());
+    public static final PIMXType<String> STRING = register(new StringType());
+
+    private static <T> PIMXType<T> register(PIMXType<T> type) {
+        TYPES.put(type.getId(), type);
+        return type;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> PIMXType<T> get(String id) {
+        PIMXType<?> type = TYPES.get(id);
+        if (type == null) {
+            throw new PIMXException("Unknown type id: " + id);
+        }
+        return (PIMXType<T>) type;
+    }
 
     private PIMXTypes() {}
 }
