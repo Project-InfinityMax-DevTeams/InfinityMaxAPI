c82ffbfdb9e5c99d537699fd0161ff4c6696ba95
Yuyuto <yukkurigame0101@gmail.com>
2026-02-28 09:52:08 +0900

    Debugged

--- src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/BehaviorConnectionDsl.kt
+++ src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/BehaviorConnectionDsl.kt
@@ -75,7 +75,7 @@ class BehaviorScope {
             PacketBehaviorBinding(
                 id,
                 definition.resourceId,
-                definition.phase,
+                definition.phase.name.lowercase,
                 definition.metadata,
                 connector,
                 T::class.java
@@ -94,7 +94,7 @@ class BehaviorScope {
                 type,
                 id,
                 definition.resourceId,
-                definition.phase,
+                definition.phase.name.lowercase,
                 definition.metadata,
                 connector
             )
