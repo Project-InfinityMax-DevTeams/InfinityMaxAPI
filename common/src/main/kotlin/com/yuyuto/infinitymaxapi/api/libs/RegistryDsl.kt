ac26655e48ac40f12ee51e8cbc8eb7b5fc1e5a9d
Yuyuto <yukkurigame0101@gmail.com>
2026-03-03 22:14:40 +0900

    debugg

--- common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
+++ common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
@@ -59,7 +59,7 @@ class RegistryScope {
 
     fun <T : Any, B : Any> blockEntity(id: String, template: T, vararg blocks: B, block: BlockEntitySettings.() -> Unit = {}): T {
         val settings = BlockEntitySettings().apply(block)
-        ModRegistriesProvider.get().registerBlockEntity(id, template, blocks, Array<B>, settings)
+        ModRegistriesProvider.get().registerBlockEntity(id, template, *blocks, settings)
         return template
     }
 
