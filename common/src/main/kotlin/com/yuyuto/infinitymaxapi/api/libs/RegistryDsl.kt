e73ad6c5a53c771fb8397d1f8963a1065b63ded6
Yuyuto <yukkurigame0101@gmail.com>
2026-03-03 23:54:14 +0900

    DSL

--- common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
+++ common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
@@ -50,9 +50,9 @@ class RegistryScope {
         return template
     }
 
-    fun <T : Any, C : Any> entity(id: String, template: T, category: C, block: EntitySettings<C>.() -> Unit): T {
-        val settings = EntitySettings<C>(category).apply(block)
-        val category = requireNotNull(settings.category) { "Entity category is required" }
+    fun <T : Any, C : Any> entity(id: String, template: T, block: EntitySettings<C>.() -> Unit): T {
+        val settings = EntitySettings<C>().apply(block)
+        requireNotNull(settings.category) { "Entity category is required" }
         ModRegistriesProvider.get().registerEntity(id, template, settings)
         return template
     }
