7412015d98c6305a49f3883314c9b76a90a02b43
Yuyuto <yukkurigame0101@gmail.com>
2026-03-04 00:21:48 +0900

    dou_

--- common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
+++ common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
@@ -50,8 +50,8 @@ class RegistryScope {
         return template
     }
 
-    fun <T : Any, C : Any> entity(id: String, template: T, block: EntitySettings<C>.() -> Unit): T {
-        val settings = EntitySettings<C>().apply(block)
+    fun <T : Any, C : Any> entity(id: String, template: T, category: C, block: EntitySettings<C>.() -> Unit): T {
+        val settings = EntitySettings<C>(category).apply(block)
         ModRegistriesProvider.get().registerEntity(id, template, settings)
         return template
     }
