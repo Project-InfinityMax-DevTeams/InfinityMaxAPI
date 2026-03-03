1adecd5cc4419f3ba52fe83370f485c029c09e8f
Yuyuto <yukkurigame0101@gmail.com>
2026-03-03 22:02:02 +0900

    RegistryDsl.kt を更新

--- common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
+++ common/src/main/kotlin/com/yuyuto/infinitymaxapi/api/libs/RegistryDsl.kt
@@ -59,7 +59,7 @@ class RegistryScope {
 
     fun <T : Any, B : Any> blockEntity(id: String, template: T, vararg blocks: B, block: BlockEntitySettings.() -> Unit = {}): T {
         val settings = BlockEntitySettings().apply(block)
-        ModRegistriesProvider.get().registerBlockEntity(id, template, blocks, Array<out B>, settings)
+        ModRegistriesProvider.get().registerBlockEntity(id, template, blocks, Array<B>, settings)
         return template
     }
 
