e293ba4d4b627dd86e983aabc0b130b998e3470d
Yuyuto <yukkurigame0101@gmail.com>
2026-03-04 19:22:33 +0900

    debug3

--- forge/src/main/java/com/yuyuto/infinitymaxapi/ForgeEntrypoint.java
+++ forge/src/main/java/com/yuyuto/infinitymaxapi/ForgeEntrypoint.java
@@ -3,7 +3,7 @@ package com.yuyuto.infinitymaxapi;
 import com.yuyuto.infinitymaxapi.InfinityMaxAPI;
 import com.yuyuto.infinitymaxapi.loader.Platform;
 import com.yuyuto.infinitymaxapi.api.libs.registry.ModRegistriesProvider;
-import com.yuyuto.infinitymaxapi.forgeimpl;
+import com.yuyuto.infinitymaxapi.forgeimpl.ForgePlatform;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("infinitymaxapi")
