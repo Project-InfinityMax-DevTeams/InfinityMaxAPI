b7560ff7a6ed7c163355f19295ff3724bd15d4cd
Yuyuto <yukkurigame0101@gmail.com>
2026-03-04 18:53:38 +0900

    debug2

--- fabric/src/main/java/com/yuyuto/infinitymaxapi/FabricEntrypoint.java
+++ fabric/src/main/java/com/yuyuto/infinitymaxapi/FabricEntrypoint.java
@@ -3,6 +3,7 @@ package com.yuyuto.infinitymaxapi;
 import com.yuyuto.infinitymaxapi.InfinityMaxAPI;
 import com.yuyuto.infinitymaxapi.loader.Platform;
 import com.yuyuto.infinitymaxapi.api.libs.registry.ModRegistriesProvider;
+import com.yuyuto.infinitymaxapi.fabricimpl;
 import net.fabricmc.api.ModInitializer;
 
 public final class FabricEntrypoint implements ModInitializer {
--- forge/src/main/java/com/yuyuto/infinitymaxapi/ForgeEntrypoint.java
+++ forge/src/main/java/com/yuyuto/infinitymaxapi/ForgeEntrypoint.java
@@ -3,6 +3,7 @@ package com.yuyuto.infinitymaxapi;
 import com.yuyuto.infinitymaxapi.InfinityMaxAPI;
 import com.yuyuto.infinitymaxapi.loader.Platform;
 import com.yuyuto.infinitymaxapi.api.libs.registry.ModRegistriesProvider;
+import com.yuyuto.infinitymaxapi.forgeimpl;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("infinitymaxapi")
