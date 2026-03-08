abc3d98766d00f53ec86c31d4077a3e9434db0c6
Yuyuto <yukkurigame0101@gmail.com>
2026-03-09 07:48:06 +0900

    BlockEntityDefinition.java を更新

--- common/src/main/java/com/yuyuto/infinitymaxapi/api/registry/BlockEntityDefinition.java
+++ common/src/main/java/com/yuyuto/infinitymaxapi/api/registry/BlockEntityDefinition.java
@@ -2,21 +2,8 @@ package com.yuyuto.infinitymaxapi.api.registry;
 
 import java.util.List;
 
-public class BlockEntityDefinition {
-
-    private final String id;
-    private final List<BehaviorDefinition> behaviors;
-
-    public BlockEntityDefinition(String id, List<BehaviorDefinition> behaviors){
-        this.id = id;
-        this.behaviors = behaviors;
-    }
-
-    public String getId(){
-        return id;
-    }
-
-    public List<BehaviorDefinition> getBehaviors(){
-        return behaviors;
-    }
-}
+public record BlockEntityDefinition(
+        String id,
+        List<BehaviorDefinition> behaviors,
+        Supplier<BlockEntityRendererFactory<?>> renderer
+) {}
