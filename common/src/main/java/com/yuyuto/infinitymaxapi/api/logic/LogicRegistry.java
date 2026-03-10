c4958bb08d0bd88da5bcd0087fd929c98f10a2e3
Yuyuto <yukkurigame0101@gmail.com>
2026-03-10 21:49:51 +0900

    LogicRegistry.java を更新

--- common/src/main/java/com/yuyuto/infinitymaxapi/api/logic/LogicRegistry.java
+++ common/src/main/java/com/yuyuto/infinitymaxapi/api/logic/LogicRegistry.java
@@ -1,6 +1,5 @@
 package com.yuyuto.infinitymaxapi.api.logic;
 
-
 import com.yuyuto.infinitymaxapi.api.behavior.BehaviorContext;
 
 import java.util.Map;
@@ -8,26 +7,27 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class LogicRegistry {
 
-    private static final Map<String, Logic> LOGICS = new ConcurrentHashMap<>();
+    private static final Map<Logic, Logic> LOGICS = new ConcurrentHashMap<>();
 
     private LogicRegistry(){}
 
-    //Logic登録
+    // Logic登録
     public static void register(Logic logic){
-        LOGICS.put(logic.id(), logic);
+        LOGICS.put(logic, logic);
     }
 
-    //logic取得
-    public static Logic get(String id){
-        return LOGICS.get(id);
+    // Logic取得
+    public static Logic get(Logic logic){
+        return LOGICS.get(logic);
     }
 
-    //logic実行
-    public static void execute(String id, BehaviorContext ctx, Object payload){
-        Logic logic = LOGICS.get(id);
+    // Logic実行
+    public static void execute(Logic logic, BehaviorContext ctx, Object payload){
 
-        if (logic != null){
-            logic.execute(ctx, payload);
+        Logic l = LOGICS.get(logic);
+
+        if(l != null){
+            l.execute(ctx, payload);
         }
     }
 }
