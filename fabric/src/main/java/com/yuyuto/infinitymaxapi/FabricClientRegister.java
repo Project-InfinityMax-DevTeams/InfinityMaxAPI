b36759a4fc029808a053c069962edc2a2180c885
Yuyuto <yukkurigame0101@gmail.com>
2026-03-12 07:37:10 +0900

    FabricClientRegister.java を更新

--- fabric/src/main/java/com/yuyuto/infinitymaxapi/FabricClientRegister.java
+++ fabric/src/main/java/com/yuyuto/infinitymaxapi/FabricClientRegister.java
@@ -3,6 +3,8 @@ package com.yuyuto.infinitymaxapi;
 import com.yuyuto.infinitymaxapi.api.registry.BlockDefinition;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 
@@ -11,7 +13,7 @@ public class FabricClientRegister {
     public static void registerRenderers(List<BlockDefinition> definitions){
         for(BlockDefinition def : definitions){
             if(def.hasRenderer()){
-                BlockEntityType<?> type = FabricRegistry.getBlockEntityType(def.getId());
+                BlockEntityType<?> type = Registries.BLOCK_ENTITY_TYPE.get(new Identifier(def.getId()));
                 var factory = RendererRegistry.get(def.getRenderer());
                 BlockEntityRendererRegistry.register(type, factory);
             }
