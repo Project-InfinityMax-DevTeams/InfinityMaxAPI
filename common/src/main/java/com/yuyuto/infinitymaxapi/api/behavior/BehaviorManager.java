75c2abb44cc958b277e918b93cab2e4247438d69
Yuyuto <yukkurigame0101@gmail.com>
2026-03-11 23:24:49 +0900

    BehaviorManager.java を更新

--- common/src/main/java/com/yuyuto/infinitymaxapi/api/behavior/BehaviorManager.java
+++ common/src/main/java/com/yuyuto/infinitymaxapi/api/behavior/BehaviorManager.java
@@ -24,7 +24,7 @@ public final class BehaviorManager {
                     behavior.metadata()
             );
 
-            LogicManager.run(behavior.logic(), context);
+            LogicManager.run(behavior.logicId(), context);
         }
     }
 }
